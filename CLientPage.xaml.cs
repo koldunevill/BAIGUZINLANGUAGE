1bc0a2b9fa6430e682dacda599e87dcd292c7ca3
egorb <egorb@DESKTOP-1F4FHUE>
2024-03-26 17:29:31 +0500

    Добавьте файлы проекта.

---
+++ CLientPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BAIGUZINLANGUAGE
+{
+    /// <summary>
+    /// Логика взаимодействия для CLientPage.xaml
+    /// </summary>
+    public partial class CLientPage : Page
+    {
+        public CLientPage()
+        {
+            InitializeComponent();
+
+            List<Client> currentClients = BaiguzinLanguageEntities.GetContext().Client.ToList();
+
+            ClientListView.ItemsSource = currentClients;
+        }
+    }
+}
